d8aebfef2d336043769e950186798793e9098957
zimiki <marmonka@gmail.com>
2020-01-10 12:52:36 +0500

    Исправляет замечания 3

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**.{png,jpg,svg}",
+    "source/img/**.{png,jpg,svg,webp}",
     "!source/img/icon-social*.svg",
     "source/*.ico"
   ], {
